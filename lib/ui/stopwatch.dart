97ae9c1c0c09208ed049bf53ee41a8205be2e186
Kasun Hasanga <kasunhasanga96@gmail.com>
2021-12-09 00:13:08 +0530

    make timer dynamically show

--- lib/ui/stopwatch.dart
+++ lib/ui/stopwatch.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:stopwatch_flutter/ui/elapsed_time_text_basic.dart';
 
@@ -7,10 +9,27 @@ class Stopwatch extends StatefulWidget {
 }
 
 class _StopwatchState extends State<Stopwatch> {
+
+  late DateTime _initialTime;
+  Duration _elapsed =Duration.zero;
+
+   
+
+  @override
+  void initState() {
+    super.initState();
+    _initialTime =DateTime.now();
+    Timer.periodic(Duration(microseconds: 50),(_){
+      final now =DateTime.now();
+      setState(() {
+        _elapsed=now.difference(_initialTime);
+      });
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return ElapsedTimeTextBasic(
-      elapsed: Duration(seconds: 5),
+      elapsed: _elapsed,
     );
   }
 }
