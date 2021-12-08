5056efd864478f2d606ef540f577218d8319b041
Kasun Hasanga <kasunhasanga96@gmail.com>
2021-12-09 00:26:11 +0530

    make timer dynamically show with Ticker

--- lib/ui/stopwatch.dart
+++ lib/ui/stopwatch.dart
@@ -1,34 +1,42 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:stopwatch_flutter/ui/elapsed_time_text_basic.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:stopwatch_flutter/ui/elapsed_time_text.dart';
 
 class Stopwatch extends StatefulWidget {
   @override
   _StopwatchState createState() => _StopwatchState();
 }
 
-class _StopwatchState extends State<Stopwatch> {
-
+class _StopwatchState extends State<Stopwatch>
+    with SingleTickerProviderStateMixin {
   late DateTime _initialTime;
-  Duration _elapsed =Duration.zero;
+  Duration _elapsed = Duration.zero;
+  late final Ticker _ticker;
 
-   
+  @override
+  void dispose() {
+    super.dispose();
+    _ticker.dispose();
+  }
 
   @override
   void initState() {
     super.initState();
-    _initialTime =DateTime.now();
-    Timer.periodic(Duration(microseconds: 50),(_){
-      final now =DateTime.now();
+    _initialTime = DateTime.now();
+    _ticker=this.createTicker((elapsed) {
+      final now = DateTime.now();
       setState(() {
-        _elapsed=now.difference(_initialTime);
+        _elapsed = now.difference(_initialTime);
       });
     });
+    _ticker.start();
   }
+
   @override
   Widget build(BuildContext context) {
-    return ElapsedTimeTextBasic(
+    return ElapsedTimeText(
       elapsed: _elapsed,
     );
   }
